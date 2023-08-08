77ad148373746efaab1d31faf9bca0a71c7bbd29
MRP <ipazooki@gmail.com>
2023-08-08 05:58:43 +0100

    Project init

---
+++ WaitWithTimeout.Tests/UtilityTests.cs
@@ -0,0 +1,45 @@
+namespace WaitWithTimeout.Tests;
+
+public class UtilityTests
+{
+    [Fact]
+    public async Task WaitWithTimeoutAsync_ShouldThrowTimeoutException_WhenTaskIsNotCompletedWithinTimeout()
+    {
+        // Arrange
+        var timeout = TimeSpan.FromSeconds(1);
+        var task = Task.Delay(5000);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<TimeoutException>(() => task.WaitWithTimeoutAsync(timeout));
+    }
+
+    [Fact]
+    public async Task WaitWithTimeoutAsync_ShouldReturnResult_WhenTaskIsCompletedWithinTimeout()
+    {
+        // Arrange
+        var timeout = TimeSpan.FromSeconds(5);
+        var expected = 42;
+        var task = Task.FromResult(expected);
+
+        // Act
+        var actual = await task.WaitWithTimeoutAsync(timeout);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task WaitWithTimeoutAsync_ShouldReturnResult_WhenValueTaskIsCompletedWithinTimeout()
+    {
+        // Arrange
+        var timeout = TimeSpan.FromSeconds(5);
+        var expected = 42;
+        var task = new ValueTask<int>(expected);
+
+        // Act
+        var actual = await task.WaitWithTimeoutAsync(timeout);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ WaitWithTimeout/Utility.cs
@@ -0,0 +1,44 @@
+﻿namespace WaitWithTimeout;
+
+public static class Utility
+{
+    private const string ExceptionMessage = "The operation has timed out.";
+
+    public static async Task<T> WaitWithTimeoutAsync<T>(this Task<T> task, TimeSpan timeout, string exceptionMessage = ExceptionMessage)
+    {
+        using var cts = new CancellationTokenSource(timeout);
+
+        var completedTask = await Task.WhenAny(task, Task.Delay(-1, cts.Token));
+
+        if (completedTask == task)
+        {
+            cts.Cancel();
+
+            return await task;
+        }
+        else
+        {
+            throw new TimeoutException(exceptionMessage);
+        }
+    }
+
+    public static async Task WaitWithTimeoutAsync(this Task task, TimeSpan timeout, string exceptionMessage = ExceptionMessage)
+    {
+        using var cts = new CancellationTokenSource(timeout);
+
+        var completedTask = await Task.WhenAny(task, Task.Delay(-1, cts.Token));
+
+        if (completedTask == task)
+        {
+            cts.Cancel();
+
+            await task;
+        }
+        else
+        {
+            throw new TimeoutException(exceptionMessage);
+        }
+    }
+
+    public static async ValueTask<T> WaitWithTimeoutAsync<T>(this ValueTask<T> task, TimeSpan timeout) => await task.AsTask().WaitWithTimeoutAsync(timeout);
+}
